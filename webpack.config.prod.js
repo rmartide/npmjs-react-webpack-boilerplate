05a141b88b4f2667a9a6ec54857e774ded444a93
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2018-11-29 17:15:09 +0100

    changing filename

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/componente.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.min.js',
+        filename: 'packagename.min.js',
         libraryTarget: 'umd',
         library: 'libraryName',
         umdNamedDefine: true
